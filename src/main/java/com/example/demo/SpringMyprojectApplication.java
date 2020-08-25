1e8a4890f858234d14924e57d3b6db18eebacc9b
ryuunosuke.ishikawa <ryuunosuke113666@yahoo.co.jp>
2020-08-25 16:08:41 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/SpringMyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMyprojectApplication.class, args);
+	}
+
+}
