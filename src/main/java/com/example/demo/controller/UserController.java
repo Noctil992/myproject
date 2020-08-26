d850b26aa5d69145b6a9445e68b0aba7a355070f
ryuunosuke.ishikawa <ryuunosuke113666@yahoo.co.jp>
2020-08-26 14:52:57 +0900

    Add new html

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,8 +3,9 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.demo.models.Users;
 import com.example.demo.repository.UsersRepository;
 
 @Controller
@@ -13,7 +14,7 @@ public class UserController {
     UsersRepository usersrepository;
     
     @GetMapping("/users/new")
-    public String newUsersPage() {
+    public String newUsersPage(@ModelAttribute Users users) {
         return "users/newUser";
     }
     
